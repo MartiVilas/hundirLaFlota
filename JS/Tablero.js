d0fcd8b5ea3708153173578313000e582e5cf432
Martí Vilàs <marti.vilas14@gmail.com>
2025-03-10 10:46:28 +0100

    create classes

--- JS/Tablero.js
+++ JS/Tablero.js
@@ -1,3 +1,18 @@
-class Tablero{
+const json = `[
+   { "name": "Portaaviones", "size": 5 },
+   { "name": "Acorazado", "size": 4 },
+   { "name": "Crucero", "size": 3 },
+   { "name": "Submarino", "size": 3 },
+   { "name": "Destructor", "size": 2 }
+]`;
 
+//casillas
+export class Tablero {}
+
+// x,y,ocupada,impactada,nombreBarco
+class casilla {
+  x;
+  y;
+  impactada;
+  nombreBarco;
 }
